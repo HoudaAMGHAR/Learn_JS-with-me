1ebe926fbdd6a27996d3d94c52ad4e7cbfd20db9
AMGHAR Houda <124742406+HoudaAMGHAR@users.noreply.github.com>
2023-05-01 21:06:15 +0200

    Add files via upload

---
+++ Arrays.js
@@ -0,0 +1,15 @@
+//Arrays
+var firstName="Houda"
+var names=['houda',
+    'mama',
+    'father',
+    'Aicha',
+    'ahmed']
+console.log(names);
+console.log('index 0-'+names[0]);
+console.log('index 1-'+ names[1]);
+console.log('index 2-'+names[2]);
+console.log('index 3-'+names[3]);
+console.log('index 4-'+names[4]);
+console.log('index 5-'+names[5]);
+
